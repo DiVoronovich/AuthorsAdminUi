11d022541fb6e8a4ceaa5ba448dba8a23ee20a94
Dziyana Varanovich <dvoronovich@scnsoft.com>
2022-01-23 23:01:14 +0300

    Added addition photo for author

--- ScienceSoft/AuthorsAdminUi/Controller/Adminhtml/Literature/Image/Upload.php
+++ ScienceSoft/AuthorsAdminUi/Controller/Adminhtml/Literature/Image/Upload.php
@@ -7,11 +7,11 @@ use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Downloadable\Helper\File;
 use Magento\Framework\App\Action\HttpPostActionInterface;
-use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\Result\RawFactory;
 use Magento\Framework\Controller\ResultFactory;
-use Magento\Framework\Controller\ResultInterface;
 use Magento\MediaStorage\Helper\File\Storage\Database;
 use Magento\MediaStorage\Model\File\UploaderFactory;
+use ScienceSoft\Authors\Block\Image;
 
 class Upload extends Action implements HttpPostActionInterface
 {
@@ -33,25 +33,42 @@ class Upload extends Action implements HttpPostActionInterface
     protected File $fileHelper;
 
     /**
+     * @var RawFactory
+     */
+    protected $resultRawFactory;
+
+    /**
+     * @var Image
+     */
+    private Image $image;
+
+    /**
      * @param UploaderFactory $uploaderFactory
      * @param Context $context
      * @param Database $storageDatabase
      * @param File $fileHelper
+     * @param RawFactory $resultRawFactory
+     * @param Image $image
      */
     public function __construct(
         UploaderFactory $uploaderFactory,
         Context $context,
         Database $storageDatabase,
-        File $fileHelper
+        File $fileHelper,
+        RawFactory $resultRawFactory,
+        Image $image
     ) {
         parent::__construct($context);
         $this->uploaderFactory = $uploaderFactory;
         $this->storageDatabase = $storageDatabase;
         $this->fileHelper = $fileHelper;
+        $this->resultRawFactory = $resultRawFactory;
+        $this->image = $image;
     }
 
     /**
-     * @return ResponseInterface|ResultInterface
+     * @return \Magento\Framework\Controller\Result\Raw
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
     public function execute()
     {
@@ -64,6 +81,7 @@ class Upload extends Action implements HttpPostActionInterface
             $relativePath = rtrim($tmpPath, '/') . '/' . ltrim($result['file'], '/');
             $this->storageDatabase->saveFile($relativePath);
         }
+        $result['url'] = $this->image->getPathImage('/' . $result['file']);
         return $this->resultFactory->create(ResultFactory::TYPE_JSON)->setData($result);
     }
 }
